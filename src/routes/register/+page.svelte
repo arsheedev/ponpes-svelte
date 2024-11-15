03cf77a6cc7c00302f76da6d19e98c00e3e5ad6d
Riyan Fanani <ogickarecka1108@gmail.com>
2024-11-16 05:24:42 +0700

    add: register user page

---
+++ src/routes/register/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import type { PageData } from './$types'
+	import RegisterUserForm from './RegisterUserForm.svelte'
+
+	let { data }: { data: PageData } = $props()
+</script>
+
+<RegisterUserForm data={data.form} />

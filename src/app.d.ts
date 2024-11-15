c997238e9503017f095dbfd36308f67fd03af3ec
Riyan Fanani <ogickarecka1108@gmail.com>
2024-11-16 05:24:00 +0700

    add: local session type

--- src/app.d.ts
+++ src/app.d.ts
@@ -3,11 +3,13 @@
 declare global {
 	namespace App {
 		// interface Error {}
-		// interface Locals {}
+		interface Locals {
+			session: { id: string; username: string; role: 'ADMIN' | 'USER' } | null
+		}
 		// interface PageData {}
 		// interface PageState {}
 		// interface Platform {}
 	}
 }
 
-export {};
+export {}

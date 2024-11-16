5adfc7b6b3c09b550f0f9e893902a8e7f63b9ca4
Riyan Fanani <ogickarecka1108@gmail.com>
2024-11-16 07:55:45 +0700

    add: initial admin page

---
+++ src/lib/components/team-switcher.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import * as Sidebar from '$lib/components/ui/sidebar/index.js'
+	import ThemeToggle from './theme-toggle.svelte'
+
+	// This should be `Component` after lucide-svelte updates types
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	let { teams }: { teams: { name: string; logo: any; plan: string }[] } = $props()
+
+	let activeTeam = $state(teams[0])
+</script>
+
+<Sidebar.Menu>
+	<Sidebar.MenuItem>
+		<Sidebar.MenuButton
+			size="lg"
+			class="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+		>
+			<div
+				class="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground"
+			>
+				<activeTeam.logo class="size-4" />
+			</div>
+			<div class="grid flex-1 text-left text-sm leading-tight">
+				<span class="truncate font-semibold">
+					{activeTeam.name}
+				</span>
+				<span class="truncate text-xs">{activeTeam.plan}</span>
+			</div>
+			<ThemeToggle />
+		</Sidebar.MenuButton>
+	</Sidebar.MenuItem>
+</Sidebar.Menu>
